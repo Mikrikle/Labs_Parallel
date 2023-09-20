e65e21f37876461735f48c989c1bc03749e65b37
Mihail <mikhail.perov.a@gmail.com>
2023-09-20 16:40:38 +0300

    Complete

--- lab_002/Program.cs
+++ lab_002/Program.cs
@@ -4,14 +4,69 @@
 // программы в каждом случае.
 
 using lab_002;
+using System.Diagnostics;
 
 const int NumberOfStrings = 300;
 const int StringLength = 200000;
 var N = new int[]{ 1, 2, 4, 10 };
 
-
 var array = Enumerable.Range(0, NumberOfStrings)
     .Select(_ => StringGenerator.GenerateNumericString(StringLength))
     .ToArray();
 
-Console.WriteLine(array);
+var locker = new object();
+foreach (var numberOfThreads in N)
+{
+    int threadSliceSize = NumberOfStrings / numberOfThreads;
+    int[] threadEvenCounts = new int[numberOfThreads];
+    int[] threadOddCounts = new int[numberOfThreads];
+
+    List<Thread> threads = new List<Thread>();
+
+    for (int i = 0; i < numberOfThreads; i++)
+    {
+        int localIndex = i;
+        Thread thread = new Thread(() =>
+        {
+            int start = localIndex * threadSliceSize;
+            int end = (localIndex == numberOfThreads - 1) ? NumberOfStrings : start + threadSliceSize;
+
+            for (int lineIndex = start; lineIndex < end; lineIndex++)
+            {
+                foreach (var c in array[lineIndex])
+                {
+                    if ((c - '0') % 2 == 0)
+                    {
+                        threadEvenCounts[localIndex]++;
+                    }
+                    else
+                    {
+                        threadOddCounts[localIndex]++;
+                    }
+                }
+            }
+        });
+        threads.Add(thread);
+    }
+
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    foreach (var thread in threads)
+    {
+        thread.Start();
+    }
+    foreach (var thread in threads)
+    {
+        thread.Join();
+    }
+    int totalEvenCount = threadEvenCounts.Sum();
+    int totalOddCount = threadOddCounts.Sum();
+    stopwatch.Stop();
+
+    Console.WriteLine($"Number of even: {totalEvenCount}");
+    Console.WriteLine($"Number of odd: {totalOddCount}");
+    Console.WriteLine($"Threads: {numberOfThreads}");
+    Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds} ms");
+    Console.WriteLine();
+}
+
